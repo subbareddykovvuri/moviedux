3a28142751eca9bdecf9dcf8acf9bfdbe04bafca
Manikanta Satya Subbareddy Kovvuri <subbareddy934@gmail.com>
2024-09-01 12:42:42 +0530

    Search state and search bar

--- src/components/MoviesGrid.tsx
+++ src/components/MoviesGrid.tsx
@@ -2,27 +2,33 @@ import { useState, useEffect } from 'react';
 import '../styles.css'
 import MovieCard from './MovieCard';
 
-export default function MoviesGrid(){
-    
-    const [movies, setMovies] = useState([]);
+export default function MoviesGrid() {
 
-    useEffect(()=>{
+    const [movies, setMovies] = useState([]);
+    const [searchTerm, setSearchTerm] = useState("");
+
+    useEffect(() => {
         // const m = ["a","b","c"];
         // Getting Error in Visual Studio but working fine in APP
         // setMovies(m);
 
         fetch("movies.json")
-        .then(response => response.json())
-        .then(data => setMovies(data))
-    },[]);
+            .then(response => response.json())
+            .then(data => setMovies(data))
+    }, []);
 
-    return(
-        <div className='movies-grid'>
-            {
-                movies.map(movie => (
-                    <MovieCard movie={movie} key={movie.id}/>
-                ))
-            }
+    return (
+
+        <div>
+            <input  type='text' className='search-input' placeholder='Search Movies ...'/>
+            <div className='movies-grid'>
+                {
+                    movies.map(movie => (
+                        <MovieCard movie={movie} key={movie.id} />
+                    ))
+                }
+            </div>
         </div>
+
     );
 }
