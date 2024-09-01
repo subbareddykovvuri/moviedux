0253bc1fe287c6ce6f46210898c664cf6e1a58dd
Manikanta Satya Subbareddy Kovvuri <subbareddy934@gmail.com>
2024-09-01 12:36:45 +0530

    Dynamic rating color

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -7,12 +7,22 @@ export default function MovieCard({movie}) {
         e.target.src = "images/default.jpg";
     }
 
+    const getRatingClass = (rating) =>  {
+        if(rating>=8){
+            return 'rating-good';
+        }
+        else if(rating>=5 && rating<8){
+            return 'rating-ok';
+        }
+        return 'rating-bad';
+    };
+
     return (
         <div key={movie.id} className='movie-card'>
             <img src={`images/${movie.image}`} alt={movie.title}  onError={handleErro}/>
             <div className='movie-card-info'>
                 <h3 className='movie-card-genre'>{movie.title}</h3>
-                <p className='movie-card-rating'>{movie.rating}</p>
+                <p className={`movie-card-rating ${getRatingClass(movie.rating)}`}>{movie.rating}</p>
             </div>
         </div>
     )
