81ebf9e76bb628947987b2846651b793bbf939c2
Manikanta Satya Subbareddy Kovvuri <subbareddy934@gmail.com>
2024-09-01 12:53:11 +0530

    Search bar logic

--- src/components/MoviesGrid.tsx
+++ src/components/MoviesGrid.tsx
@@ -17,13 +17,21 @@ export default function MoviesGrid() {
             .then(data => setMovies(data))
     }, []);
 
+    const handleSearchChange = (e)=>{
+        setSearchTerm(e.target.value)
+    }
+
+    const filteredMovies = movies.filter(movie => 
+        movie.title.toLowerCase().includes(searchTerm.toLowerCase())
+    )
+
     return (
 
         <div>
-            <input  type='text' className='search-input' placeholder='Search Movies ...'/>
+            <input  type='text' className='search-input' placeholder='Search Movies ...' value={searchTerm} onChange={handleSearchChange}/>
             <div className='movies-grid'>
                 {
-                    movies.map(movie => (
+                    filteredMovies.map(movie => (
                         <MovieCard movie={movie} key={movie.id} />
                     ))
                 }
