726c502516233a2ca48b6a5b10b14e954ef7959a
Manikanta Satya Subbareddy Kovvuri <subbareddy934@gmail.com>
2024-09-01 12:26:52 +0530

    Error handling for image Loading

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -2,9 +2,14 @@ import '../styles.css'
 
 
 export default function MovieCard({movie}) {
+
+    const handleErro =(e) => {
+        e.target.src = "images/default.jpg";
+    }
+
     return (
         <div key={movie.id} className='movie-card'>
-            <img src={`images/${movie.image}`} alt={movie.title} />
+            <img src={`images/${movie.image}`} alt={movie.title}  onError={handleErro}/>
             <div className='movie-card-info'>
                 <h3 className='movie-card-genre'>{movie.title}</h3>
                 <p className='movie-card-rating'>{movie.rating}</p>
